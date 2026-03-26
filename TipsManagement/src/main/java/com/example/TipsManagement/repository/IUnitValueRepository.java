351336ff9da68f3212fb49b26e2bd051f59ee507
Darlei <darleifeix97@gmail.com>
2026-03-26 12:06:30 -0300

    Ajustes em comentários e remoçaõ de imports não utilizados.

--- TipsManagement/src/main/java/com/example/TipsManagement/repository/IUnitValueRepository.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/repository/IUnitValueRepository.java
@@ -3,17 +3,11 @@ package com.example.TipsManagement.repository;
 import com.example.TipsManagement.model.UnitValue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IUnitValueRepository extends JpaRepository<UnitValue, Long> {
-//    findTopBy > retorna apenas um registro
-//    StartDateLessThan > somente retorna datas menores que a passada
-//    OrderByStartDateDesc > ordena pela data de maneira decrescente
-    //IdDesc para ordenar por ID e em caso de datas iguais pegar o ultimo cadastrado
     Optional<UnitValue> findTopByUsuarioIdOrderByIdDesc(Long userId);
     List<UnitValue> findAllByUsuarioId(Long userId);
 
