12eeff2895271e4058ab8095b603ec4cad345412
Darlei <darleifeix97@gmail.com>
2026-03-26 12:04:25 -0300

    Ajustados metodos de POST/GET de betO]HouseController para passar o id du]o usuario logado, e criado rotas de PUT/DELETE.

--- TipsManagement/src/main/java/com/example/TipsManagement/controller/BetHouseController.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/controller/BetHouseController.java
@@ -1,10 +1,12 @@
 package com.example.TipsManagement.controller;
 
+import com.example.TipsManagement.model.LoggedUser;
 import com.example.TipsManagement.model.dto.Request.BetHouseRequest;
-import com.example.TipsManagement.model.BetHouse;
+import com.example.TipsManagement.model.dto.Response.BetHouseResponse;
 import com.example.TipsManagement.service.BetHouseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,14 +19,25 @@ public class BetHouseController {
     }
 
     @PostMapping
-    public ResponseEntity<BetHouse> saveBetHouse(@RequestBody BetHouseRequest betHouseRequest){
+    public ResponseEntity<BetHouseResponse> saveBetHouse(@AuthenticationPrincipal LoggedUser usuario, @RequestBody BetHouseRequest betHouseRequest){
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(betHouseService.save(betHouseRequest));
+                .body(betHouseService.save(usuario.getId(), betHouseRequest));
     }
 
     @GetMapping
-    public ResponseEntity<Object> listAllBetHouse(){
+    public ResponseEntity<Object> listAllBetHouse(@AuthenticationPrincipal LoggedUser usuario){
         return ResponseEntity.status(HttpStatus.OK)
-                .body(betHouseService.listAll());
+                .body(betHouseService.listAll(usuario.getId()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BetHouseResponse> editBetHouse(@AuthenticationPrincipal LoggedUser usuario, @PathVariable Long id, @RequestBody BetHouseRequest betHouseRequest){
+        return ResponseEntity. status(HttpStatus.OK)
+                .body(betHouseService.edit(usuario.getId(),id, betHouseRequest));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBetHouse(@AuthenticationPrincipal LoggedUser usuario, @PathVariable Long id){
+        betHouseService.delete(usuario.getId(), id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
