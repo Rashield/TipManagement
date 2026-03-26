3db40e58de155180d9fab187bff157ce8251d257
Darlei <darleifeix97@gmail.com>
2026-03-26 12:05:51 -0300

    Implementada lógica para CRUD de BetHouse no Service.

--- TipsManagement/src/main/java/com/example/TipsManagement/repository/IBetHouseRepository.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/repository/IBetHouseRepository.java
@@ -3,6 +3,11 @@ package com.example.TipsManagement.repository;
 import com.example.TipsManagement.model.BetHouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IBetHouseRepository extends JpaRepository<BetHouse, Long> {
-    boolean existsByName(String name);
+    boolean existsByUsuarioIdAndName(Long userId, String name);
+    List<BetHouse> findAllByUsuarioId(Long userId);
+    Optional<BetHouse> findByIdAndUsuarioId(Long id, Long userId);
 }
--- TipsManagement/src/main/java/com/example/TipsManagement/service/BetHouseService.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/service/BetHouseService.java
@@ -3,8 +3,11 @@ package com.example.TipsManagement.service;
 import com.example.TipsManagement.Exception.BadRequestException;
 import com.example.TipsManagement.Exception.BusinessException;
 import com.example.TipsManagement.Exception.NotFoundException;
+import com.example.TipsManagement.model.Usuario;
 import com.example.TipsManagement.model.dto.Request.BetHouseRequest;
 import com.example.TipsManagement.model.BetHouse;
+import com.example.TipsManagement.model.dto.Response.BetHouseResponse;
+import com.example.TipsManagement.model.dto.Response.TipsterResponse;
 import com.example.TipsManagement.repository.IBetHouseRepository;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
@@ -20,22 +23,49 @@ public class BetHouseService {
         this.mapper = mapper;
     }
 
-    public BetHouse save(BetHouseRequest betHouseRequest){
-        if(betHouseRepository.existsByName(betHouseRequest.getName())){
+    public BetHouseResponse save(Long userId, BetHouseRequest betHouseRequest){
+        if(betHouseRepository.existsByUsuarioIdAndName(userId, betHouseRequest.getName())){
             throw new BusinessException("Já existe uma casa de apostas com esse nome.");
         }
         if(betHouseRequest.getName().length()<3){
             throw new BadRequestException("Número de caracteres insuficiente (min. 3)");
         }
-        BetHouse betHouse = mapper.convertValue(betHouseRequest,BetHouse.class);
-        return betHouseRepository.save(betHouse);
+        BetHouse betHouse = new BetHouse();
+        betHouse.setName(betHouseRequest.getName());
+        Usuario usuario = new Usuario();
+        usuario.setId(userId);
+        betHouse.setUsuario(usuario);
+        return mapper.convertValue(betHouseRepository.save(betHouse), BetHouseResponse.class);
     }
 
-    public List<BetHouse> listAll(){
-        List<BetHouse> list = betHouseRepository.findAll();
+    public List<BetHouseResponse> listAll(Long userId){
+        List<BetHouse> list = betHouseRepository.findAllByUsuarioId(userId);
         if(list.isEmpty()){
             throw new NotFoundException("Nenhuma casa de apostas cadastrada.");
         }
-        return list;
+        return list.stream()
+                .map(betHouse -> mapper.convertValue(betHouse, BetHouseResponse.class))
+                .toList();
+    }
+
+    public BetHouseResponse edit(Long userId, Long id, BetHouseRequest betHouseRequest){
+        if(betHouseRepository.existsByUsuarioIdAndName(userId, betHouseRequest.getName())){
+            throw new BusinessException("Já existe uma casa de apostas com esse nome.");
+        }
+        if(betHouseRequest.getName().length()<3){
+            throw new BadRequestException("Número de caracteres insuficiente (min. 3)");
+        }
+        BetHouse betHouse = betHouseRepository.findByIdAndUsuarioId(id, userId)
+                .orElseThrow(() ->
+                        new NotFoundException("Não existe casa de Aposta com esse Id."));
+        betHouse.setName(betHouseRequest.getName());
+        return mapper.convertValue(betHouseRepository.save(betHouse), BetHouseResponse.class);
+    }
+
+    public void delete(Long userId, Long id){
+        BetHouse betHouse = betHouseRepository.findByIdAndUsuarioId(id, userId)
+                .orElseThrow(() ->
+                        new NotFoundException("Não existe casa de Aposta com esse Id."));
+        betHouseRepository.delete(betHouse);
     }
 }
