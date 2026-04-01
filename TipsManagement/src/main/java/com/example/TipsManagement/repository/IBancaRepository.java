33ea9ac5e46d1834dc5426f2923269ed29671054
Darlei <darleifeix97@gmail.com>
2026-04-01 10:03:20 -0300

    Criada lógica no Service para buscar bancas por usuario e metodos necessários para isso no Repository.

--- TipsManagement/src/main/java/com/example/TipsManagement/repository/IBancaRepository.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/repository/IBancaRepository.java
@@ -4,7 +4,11 @@ import com.example.TipsManagement.model.Banca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IBancaRepository extends JpaRepository<Banca, Long> {
-
+    List<Banca> findAllByUsuarioId(Long userId);
+    Optional<Banca> findByIdAndUsuarioId(Long id, Long userId);
 }
--- TipsManagement/src/main/java/com/example/TipsManagement/service/BancaService.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/service/BancaService.java
@@ -3,13 +3,17 @@ package com.example.TipsManagement.service;
 import com.example.TipsManagement.Exception.NotFoundException;
 import com.example.TipsManagement.model.Banca;
 import com.example.TipsManagement.model.BetHouse;
+import com.example.TipsManagement.model.Tipster;
 import com.example.TipsManagement.model.dto.Request.BancaRequest;
 import com.example.TipsManagement.model.dto.Response.BancaResponse;
 import com.example.TipsManagement.model.dto.Response.BetHouseResponse;
+import com.example.TipsManagement.model.dto.Response.TipsterResponse;
 import com.example.TipsManagement.repository.IBancaRepository;
 import com.example.TipsManagement.repository.IBetHouseRepository;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 @Service
 public class BancaService {
@@ -38,5 +42,23 @@ public class BancaService {
         bancaResponse.setBalance(savedBanca.getBalance());
         bancaResponse.setId(savedBanca.getId());
         return bancaResponse;
+    }
+
+    public List<BancaResponse> listAll(Long userId){
+        List<Banca> list = bancaRepository.findAllByUsuarioId(userId);
+        if(list.isEmpty()){
+            throw new NotFoundException("Não existe Banca cadastrada.");
+        }
+        //Retorna a lista convertida para BancaResponse, evitando passar dados sensíveis do usuario cadastrado (senha)
+        return list.stream()
+                .map(banca -> mapper.convertValue(banca, BancaResponse.class))
+                .toList();
+    }
+
+    public BancaResponse list(Long userId, Long id){
+        Banca banca = bancaRepository.findByIdAndUsuarioId(id, userId)
+                .orElseThrow(()->
+                        new NotFoundException("Não existe Banca com os parâmetros fornecidos."));
+        return mapper.convertValue(banca, BancaResponse.class);
     }
 }
