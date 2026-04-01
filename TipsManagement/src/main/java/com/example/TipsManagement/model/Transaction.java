ddb0468c23e9a6712dd4992dd3766bb46fba448a
Darlei <darleifeix97@gmail.com>
2026-04-01 16:10:50 -0300

    Criada classe de Transaction, ira mapear os depositos/ saques realizados

---
+++ TipsManagement/src/main/java/com/example/TipsManagement/model/Transaction.java
@@ -0,0 +1,29 @@
+package com.example.TipsManagement.model;
+
+import com.example.TipsManagement.model.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    private LocalDate date;
+
+    @ManyToOne
+    private Banca banca;
+}
