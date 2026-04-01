37fac087572cdba67fe2740968f3fc5c2fc2fd51
Darlei <darleifeix97@gmail.com>
2026-04-01 16:07:07 -0300

    Criação do TransactionRepository

---
+++ TipsManagement/src/main/java/com/example/TipsManagement/repository/ITransactionRepository.java
@@ -0,0 +1,10 @@
+package com.example.TipsManagement.repository;
+
+import com.example.TipsManagement.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
