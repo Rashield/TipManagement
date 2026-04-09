3dca218686f4bc7f88e95340d0a2bc45214d8c70
Darlei <darleifeix97@gmail.com>
2026-04-08 21:54:43 -0300

    Cria metodos getBanca e createTransaction para reduzir repetição de codigo Implementada a lógica de saque, criando/salvando a transação e atualizando o saldo.

--- TipsManagement/src/main/java/com/example/TipsManagement/service/TransactionService.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/service/TransactionService.java
@@ -12,6 +12,7 @@ import com.example.TipsManagement.repository.ITransactionRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Service
@@ -28,21 +29,32 @@ public class TransactionService {
 
     @Transactional
     public TransactionResponse deposit(Long userId, Long bancaId, TransactionRequest transactionRequest){
-        Banca banca = bancaRepository.findByIdAndBetHouse_UsuarioId(bancaId, userId)
-                        .orElseThrow(()->
-                            new NotFoundException("Não existe Banca com os parâmetros fornecidos."));
+        Banca banca = getBanca(bancaId, userId); // busca banca fornecida
+        banca.deposit(transactionRequest.getAmount()); //chama o metodo de deposito da entidade Banca para depositar o valor
+        Transaction transaction = createTransaction(banca, transactionRequest.getAmount(), TransactionType.DEPOSIT);//chama a funcao para criar uma transacao
+        return transactionMapper.toResponse(transactionRepository.save(transaction));//converte a transacao para Response
+    }
 
-        banca.setBalance(banca.getBalance().add(transactionRequest.getAmount()));
+    @Transactional
+    public TransactionResponse withdraw(Long userId, Long bancaId, TransactionRequest transactionRequest){
+        Banca banca = getBanca(bancaId, userId);
+        banca.withdraw(transactionRequest.getAmount());
+        Transaction transaction = createTransaction(banca,transactionRequest.getAmount(),TransactionType.WITHDRAW);
+        return transactionMapper.toResponse(transactionRepository.save(transaction));
+    }
+    //metodo para validar se a banca existe, vai ser usado em todas transações
+    private Banca getBanca(Long bancaId, Long userId) {
+        return bancaRepository.findByIdAndBetHouse_UsuarioId(bancaId, userId)
+                .orElseThrow(() ->
+                        new NotFoundException("Não existe Banca com os parâmetros fornecidos."));
+    }
+    //metodo para criar uma transação, tbm será usada em todas, visa evitar duplicação
+    private Transaction createTransaction(Banca banca, BigDecimal amount, TransactionType type) {
         Transaction transaction = new Transaction();
-        transaction.setTransactionType(TransactionType.DEPOSIT);
-        transaction.setDate(LocalDate.now());
-        transaction.setAmount(transactionRequest.getAmount());
-        banca.setTotalDeposit(banca.getTotalDeposit().add(transactionRequest.getAmount()));
         transaction.setBanca(banca);
-
-        Transaction savedTransaction = transactionRepository.save(transaction);
-
-
-        return transactionMapper.toResponse(savedTransaction);
+        transaction.setAmount(amount);
+        transaction.setTransactionType(type);
+        transaction.setDate(LocalDate.now());
+        return transaction;
     }
 }
