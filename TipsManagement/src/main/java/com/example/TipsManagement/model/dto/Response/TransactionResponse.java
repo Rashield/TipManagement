c6b2eb209cb4b0480df183791e06ce494b7f5868
Darlei <darleifeix97@gmail.com>
2026-04-14 22:49:53 -0300

    Ajustes de nomenclturas do retorno em JSON para melhor entendimento, como id >transactionId e remoção de "Response"

--- TipsManagement/src/main/java/com/example/TipsManagement/model/dto/Response/TransactionResponse.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/model/dto/Response/TransactionResponse.java
@@ -1,16 +1,20 @@
 package com.example.TipsManagement.model.dto.Response;
 
+import com.example.TipsManagement.model.Bet;
 import com.example.TipsManagement.model.enums.TransactionType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TransactionResponse {
-    private Long id;
+    private Long transactionId;
     private BigDecimal amount;
     private TransactionType transactionType;
     private LocalDate date;
-    private BancaResponse bancaResponse;
+    private BancaResponse banca;
+    private Bet bet;
 }
