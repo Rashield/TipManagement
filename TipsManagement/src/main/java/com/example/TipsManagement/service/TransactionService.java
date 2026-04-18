df309a9d9f6294dd44bbb1792bb03bc854c48ea8
Darlei <darleifeix97@gmail.com>
2026-04-18 09:43:30 -0300

    Ediçãoda função CreateBeTransaction, recebendo somente a bet e utilizando dados de dentro dela para salvar a transacao Criação da função betWin para criar a transacao de uma aposta com status Green

--- TipsManagement/src/main/java/com/example/TipsManagement/service/TransactionService.java
+++ TipsManagement/src/main/java/com/example/TipsManagement/service/TransactionService.java
@@ -1,7 +1,6 @@
 package com.example.TipsManagement.service;
 
 import com.example.TipsManagement.Exception.BusinessException;
-import com.example.TipsManagement.Exception.NotFoundException;
 import com.example.TipsManagement.mapper.TransactionMapper;
 import com.example.TipsManagement.mapper.TransferMapper;
 import com.example.TipsManagement.model.Banca;
@@ -26,19 +25,21 @@ import java.util.UUID;
 public class TransactionService {
     private final ITransactionRepository transactionRepository;
     private final IBancaRepository bancaRepository;
+    private final BancaService bancaService;
     private final TransactionMapper transactionMapper;
     private final TransferMapper transferMapper;
 
-    public TransactionService(ITransactionRepository transactionRepository, IBancaRepository bancaRepository, TransactionMapper transactionMapper, TransferMapper transferMapper) {
+    public TransactionService(ITransactionRepository transactionRepository, IBancaRepository bancaRepository, BancaService bancaService, TransactionMapper transactionMapper, TransferMapper transferMapper) {
         this.transactionRepository = transactionRepository;
         this.bancaRepository = bancaRepository;
+        this.bancaService = bancaService;
         this.transactionMapper = transactionMapper;
         this.transferMapper = transferMapper;
     }
 
     @Transactional
     public TransactionResponse deposit(Long userId, Long bancaId, TransactionRequest transactionRequest){
-        Banca banca = getBanca(bancaId, userId); // busca banca fornecida
+        Banca banca = bancaService.getOwnedBanca(bancaId, userId); // busca banca fornecida
         banca.deposit(transactionRequest.getAmount()); //chama o metodo de deposito da entidade Banca para depositar o valor
         Transaction transaction = createTransaction(banca, transactionRequest.getAmount(), TransactionType.DEPOSIT);//chama a funcao para criar uma transacao
         return transactionMapper.toResponse(transactionRepository.save(transaction));//converte a transacao para Response
@@ -46,26 +47,33 @@ public class TransactionService {
 
     @Transactional
     public TransactionResponse withdraw(Long userId, Long bancaId, TransactionRequest transactionRequest){
-        Banca banca = getBanca(bancaId, userId);
+        Banca banca = bancaService.getOwnedBanca(bancaId, userId);
         banca.withdraw(transactionRequest.getAmount());
         Transaction transaction = createTransaction(banca,transactionRequest.getAmount(),TransactionType.WITHDRAW);
         return transactionMapper.toResponse(transactionRepository.save(transaction));
     }
 
+
     @Transactional
-    public TransactionResponse betTransaction(Long userId, Long bancaId, Bet bet){
-        Banca banca = getBanca(bancaId, userId);
-        banca.withdraw(bet.getUnitValue());
-        Transaction transaction = createTransaction(banca,bet.getUnitValue(),TransactionType.BET);
+    public Transaction createBetTransaction(Bet bet){
+        bet.getBanca().withdraw(bet.getStake());
+        Transaction transaction = createTransaction(bet.getBanca(), bet.getStake(),TransactionType.BET);
         transaction.setBet(bet);
-        return transactionMapper.toResponse(transactionRepository.save(transaction));
+        return transactionRepository.save(transaction);
+    }
+
+    @Transactional Transaction betWin(Bet bet){
+        bet.getBanca().deposit(bet.getResultValue());
+        Transaction transaction = createTransaction(bet.getBanca(), bet.getResultValue(), TransactionType.BET_WIN);
+        transaction.setBet(bet);
+        return transactionRepository.save(transaction);
     }
 
     @Transactional
     public TransferResponse transfer(Long userId, TransferRequest transferRequest){
         //validações
-        Banca toBanca = getBanca(transferRequest.getToBancaId(), userId);
-        Banca fromBanca = getBanca(transferRequest.getFromBancaId(), userId);
+        Banca toBanca = bancaService.getOwnedBanca(transferRequest.getToBancaId(), userId);
+        Banca fromBanca = bancaService.getOwnedBanca(transferRequest.getFromBancaId(), userId);
         if (toBanca.equals(fromBanca)) {
             throw new BusinessException("Não pode transferir para a mesma banca");
         }
@@ -86,14 +94,9 @@ public class TransactionService {
 
     }
 
-    //metodo para validar se a banca existe, vai ser usado em todas transações
-    private Banca getBanca(Long bancaId, Long userId) {
-        return bancaRepository.findByIdAndBetHouse_UsuarioId(bancaId, userId)
-                .orElseThrow(() ->
-                        new NotFoundException("Não existe Banca com os parâmetros fornecidos."));
-    }
 
-    //metodo para criar uma transação, tbm será usada em todas, visa evitar duplicação
+
+    //metodo para criar uma transação, será usada em todas tranasções, visa evitar duplicação
     private Transaction createTransaction(Banca banca, BigDecimal amount, TransactionType type) {
         Transaction transaction = new Transaction();
         transaction.setBanca(banca);
